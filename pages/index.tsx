8e10fce1fcebdc43e97049141e3b5aa69133d2a0
gma950 <francobocca.97@gmail.com>
2020-03-21 18:03:16 -0300

    home page lista

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,17 @@ const UsersList: FC = () => {
           </List.Item>
         ))}
       </List>
+      <div className="botones2">
+        <Link href="/crearHorario">
+          <button>Crear Horario</button>
+        </Link>
+        <br />
+        <Link href="/votaHorario">
+          <button>Votar Horario</button>
+        </Link>
+      </div>
     </div>
+    
   );
 };
 
@@ -46,18 +56,36 @@ const Index: NextPage = () => {
 
   return (
     <div>
-      <div className="back">
+      {/*<div className="back">*/}
         <div className="tll1">
           <Label>¡Tienes que haber iniciado sesión!</Label>
         </div>
-        <Link href="/crearHorario">
-          <button>Crear Horario</button>
-        </Link>
-        <br />
-        <Link href="/votaHorario">
-          <a>Votar Horario</a>
-        </Link>
-      </div>
+          <div className="box1">
+            <div className="texto1" >
+              ¡La nueva forma de organizarse!
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div className="texto2" >
+            <span style={{color: "red"}}>Sync</span>Schedule es una herramienta pensada para la orgnizacion de eventos grupales
+              <br/>
+              <br/>
+              como clases, ayudantias, citaciones, etc. 
+              <br/>
+              <br/>
+              Ideada para que sea fácil de usar para todos y de forma grauita.
+            </div>
+            <div className="pos1">
+              <Link href="/signUp">
+                <div className="feedback-button">
+                  {/*<Link href="/crearHorario">REGISTRARSE</Link>*/}
+                  REGISTRARSE
+                </div>
+              </Link>
+            </div>
+          </div>
+      {/*</div>*/}
     </div>
   );
 };
